3681ff56440454c62974600f1b673029c414101e
ijhijhijh7 <43941348+ijhijhijh7@users.noreply.github.com>
2025-02-25 23:00:46 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int T, A, B;
+    std::cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        std::cin >> A >> B;
+        std::cout << A + B << std::endl;
+    }
+        
+    return 0;
+}
