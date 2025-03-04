c346a9673eda55f69980a4638f582b793f045be7
ijhijhijh7 <43941348+ijhijhijh7@users.noreply.github.com>
2025-03-04 23:09:20 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int StringConversion(char word)
+{
+    int temp = word;;
+
+    return temp;
+}
+
+int main()
+{
+    //cin과 cout의 연결을 끊는 명령어
+    //cin의 입력이 cout의 출력을 강제로 플러시하지 않게 되어, 더 빠른 입출력이 가능
+    std::cin.tie(NULL);
+
+    //C++의 표준 입출력(cin, cout)과 
+    //C 스타일의 입출력(stdio 라이브러리의 printf, scanf) 간의 동기화를 끊는 명령어
+    //printf나 scanf와 cin, cout을 섞어 사용할 때 발생할 수 있는 문제를 예방
+    std::ios::sync_with_stdio(false);
+
+    char word;
+    std::cin >> word;
+
+    std::cout << StringConversion(word);
+
+
+    return 0;
+}
